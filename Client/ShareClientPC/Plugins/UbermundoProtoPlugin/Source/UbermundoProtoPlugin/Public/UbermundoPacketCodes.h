e4a28841cd87717922edd9da6ee29f32a2dcf810
rkeene <rmkeene@gmail.com>
2021-10-17 15:22:22 -0600

    - Some seperation of interests for UbermundoDB.uasset

--- Client/ShareClientPC/Plugins/UbermundoProtoPlugin/Source/UbermundoProtoPlugin/Public/UbermundoPacketCodes.h
+++ Client/ShareClientPC/Plugins/UbermundoProtoPlugin/Source/UbermundoProtoPlugin/Public/UbermundoPacketCodes.h
@@ -2,47 +2,73 @@
 #include "CoreMinimal.h"
 #include "Kismet/BlueprintFunctionLibrary.h"
 
+/// There are two targets of packets.  Player to and from the Server (DB) and Player to Player (Pier to Pier).
 UENUM(BlueprintType)
 enum EUbermundoPacketCodes {
+	// ============================ DB MESSAGES ==============
 	/// <summary>
-	/// If this packet is received, send back a Pong packet.
+	/// The start of Packet Codes for player to server and server to player messages. Never sent as a packet code.
 	/// </summary>
-	UBERMUNDOP2P_Ping UMETA(DisplayName = "Ping"),
-	/// <summary>
-	/// If received, send back a KeepAliveResponse.
-	/// </summary>
-	UBERMUNDOP2P_Keepalive UMETA(DisplayName = "KeepAlive"),
-	UBERMUNDOP2P_KeepAliveResponse UMETA(DisplayName = "KeepAliveResponse"),
+	UBERMUNDOPC_DB_START = 0 UMETA(DisplayName = "BD_START"),
+	UBERMUNDOPC_DB_Keepalive = 1 UMETA(DisplayName = "DB_KeepAlive"),
+	UBERMUNDOPC_DB_KeepAliveResponse = 2 UMETA(DisplayName = "DB_KeepAliveResponse"),
 	/// <summary>
 	/// A player just connected to this client. Send the world state back.
 	/// </summary>
-	UBERMUNDOP2P_PlayerConnected UMETA(DisplayName = "PlayerConnected"),
+	UBERMUNDOPC_DB_PlayerConnected = 3 UMETA(DisplayName = "DB_PlayerConnected"),
 	/// <summary>
 	/// A player left this client.
 	/// </summary>
-	UBERMUNDOP2P_PlayerDisconnected UMETA(DisplayName = "PlayerDisconnected"),
+	UBERMUNDOPC_DB_PlayerDisconnected = 4 UMETA(DisplayName = "DB_PlayerDisconnected"),
 	/// <summary>
-	/// If the player is YOU then you have been kicked forom the client back to the player's own client.
+	/// This player wants the server to kick a player. It is possible to kick your self.
 	/// </summary>
-	UBERMUNDOP2P_PlayerKicked UMETA(DisplayName = "PlayerKicked"),
+	UBERMUNDOPC_DB_AskPlayerKicked = 5 UMETA(DisplayName = "PlayerKicked"),
 	/// <summary>
-	/// Player's dynamic 3D world state update.
+	/// If the player is YOU then you have been kicked from the client back to your own client.
 	/// </summary>
-	UBERMUNDOP2P_Player3DState UMETA(DisplayName = "Player3DState"),
+	UBERMUNDOPC_DB_PlayerKicked = 6 UMETA(DisplayName = "DB_PlayerKicked"),
+	/// <summary>
+	/// A player wants the next UID for an in world object. Usually to spawn an object.
+	/// </summary>
+	UBERMUNDOPC_DB_NextUID = 7 UMETA(DisplayName = "DB_NextUID"),
+	/// <summary>
+	/// Answer to DB_NextUID with the UID
+	/// </summary>
+	UBERMUNDOPC_DB_NextUIDResponse = 8 UMETA(DisplayName = "DB_NextUIDResponse"),
+	/// <summary>
+	/// Answer to DB_PlayerConnected
+	/// </summary>
+	UBERMUNDOPC_DB_WorldContentsResponse = 9 UMETA(DisplayName = "DB_WorldContentsResponse"),
+	/// <summary>
+	/// The player is asking the DB to add an object.  Must have previously gotten a DB_NextUID.
+	/// </summary>
+	UBERMUNDOPC_DB_AddObject = 10 UMETA(DisplayName = "DB_AddObject"),
+	/// <summary>
+	/// A player is asking to delete an object from the DB.
+	/// </summary>
+	UBERMUNDOPC_DB_RemoveObject = 11 UMETA(DisplayName = "DB_RemoveObject"),
+	/// <summary>
+	/// TBD
+	/// </summary>
+	UBERMUNDOPC_DB_PlayerWOT = 12 UMETA(DisplayName = "DB_PlayerWOT"),
+
+	// ============================================== PLAYER to PLAYER MESSAGES ===============
+	/// <summary>
+	/// The start of Player ot Player messages Packet Codes. Never sent as a packet code.
+	/// </summary>
+	UBERMUNDOPC_P2P_START = 100 UMETA(DisplayName = "P2P_START"),
+	/// <summary>
+	/// Player's dynamic 3D world state update. Is also in effect a Keap Alive message.
+	/// </summary>
+	UBERMUNDOPC_P2P_Player3DState = 102 UMETA(DisplayName = "P2P_Player3DState"),
+	UBERMUNDOPC_P2P_PlayerGrabbed = 103 UMETA(DisplayName = "P2P_PlayerGrabbed"),
+	UBERMUNDOPC_P2P_PlayerReleased = 104 UMETA(DisplayName = "P2P_PlayerReleased"),
 	/// <summary>
 	/// Simple text chat message in unicode. No response needed.
 	/// </summary>
-	UBERMUNDOP2P_PlayerTextMsg UMETA(DisplayName = "PlayerTextMsg"),
-	UBERMUNDOP2P_PlayerImageMsg UMETA(DisplayName = "PlayerImageMsg"),
-	UBERMUNDOP2P_PlayerVoiceMsg UMETA(DisplayName = "PlayerVoiceMsg"),
-	/// <summary>
-	/// This client is receiving the player's known other players WOT data.
-	/// </summary>
-	UBERMUNDOP2P_PlayerWOT UMETA(DisplayName = "PlayerWOT"),
-
-	/// <summary>
-	/// If this packet is received, do nothing. Sent as a response to a Ping.
-	/// </summary>
-	UBERMUNDOP2P_Pong UMETA(DisplayName = "Pong"),
-
+	UBERMUNDOPC_P2P_PlayerTextMsg = 120 UMETA(DisplayName = "P2P_PlayerTextMsg"),
+	UBERMUNDOPC_P2P_PlayerImageMsg = 121 UMETA(DisplayName = "P2P_PlayerImageMsg"),
+	UBERMUNDOPC_P2P_PlayerVoiceMsg = 122 UMETA(DisplayName = "P2P_PlayerVoiceMsg"),
+	UBERMUNDOPC_P2P_PlayerEmoteMsg = 123 UMETA(DisplayName = "P2P_PlayerEmoteMsg"),
 };
